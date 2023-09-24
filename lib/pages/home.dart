dac72d1d7ce3483fcfd2694bb723fd0fcaf56b3d
Patricio Contreras <wolven.soen@gmail.com>
2023-09-24 14:22:36 -0600

    Fonts and home page init

---
+++ lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  //Build method is used for returning a widget, in this case
+  //we will return a scaffold widget, which is a widget that covers
+  //the whole screen, using nested widgets we can build a complex UI
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
